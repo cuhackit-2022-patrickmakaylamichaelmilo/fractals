8c9e9b16f50efd0cadaa5c463ea78e38ff58eb91
Milo Weinberg <iapetus011@gmail.com>
2022-01-29 13:06:04 -0500

    use port 8080 in run-app.sh

--- run-app.sh
+++ run-app.sh
@@ -1,3 +1,3 @@
 cd src
-poetry run uvicorn app:app --host 0.0.0.0 --port 80 --use-colors --log-level debug
+poetry run uvicorn app:app --host 0.0.0.0 --port 8080 --use-colors --log-level debug
 cd ..

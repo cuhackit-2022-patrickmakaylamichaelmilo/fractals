fae48c04f0ef5c94d3fa46b080b2e3aad2ec3d2a
Milo Weinberg <iapetus011@gmail.com>
2022-01-31 15:45:52 -0500

    update animate.py to use the nim code

--- src/testing/animate.py
+++ src/testing/animate.py
@@ -1,8 +1,21 @@
+from pathlib import Path
+from PIL import Image
 import pygifsicle
+import nimporter
 import imageio
 import time
+import sys
+import io
+import os
 
-from src_fractal import generateFractal
+# ensure modules are accessible
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+
+from nimFractal import pyGenFractal
+
+nimporter.build_nim_extensions(root=Path("../../"), danger=True)
+
 
 def main():
     print("Generating gif...")
@@ -11,14 +24,23 @@ def main():
     frames = []
 
     for i in range(1, 101):
+        print(f"Generating frame {i}/100...", end="\r")
+
         i /= 100
 
-        frames.append(generateFractal(1, 1, i, [44, 84, 165], [28, 166, 150], "sineVariation", 100000))
+        frames.append(pyGenFractal(.36, i, .49, *[0, 8, 255], *[0, 255, 30], "horseshoeVariation", 141217))
+    
+    print()
 
-    frames = frames + list(reversed(frames))
+    print("Saving...")
+
+    frames = [Image.open(io.BytesIO(f)) for f in frames]
+    frames += list(reversed(frames))
 
     with open("out.gif", "wb") as out_gif:
         imageio.mimwrite(out_gif, frames, format="gif", fps=30)
+
+    print("Optimizing with gifsicle...")
 
     pygifsicle.optimize("out.gif")
 
